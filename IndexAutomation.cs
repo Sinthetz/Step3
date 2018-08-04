eb6b8ce0cd91798bc9f289a1d17d76c7638d0060
DoT\Sinthet <sinthet@mail.com>
2018-08-04 20:44:44 +0300

    Эконом 79

--- IndexAutomation.cs
+++ IndexAutomation.cs
@@ -2753,6 +2753,69 @@ namespace Steps.NET
 
         public static void Econom79()
         {
+            doc = (ksDocument2D)kompas.Document2D();
+            DocRecPar(out ksDocumentParam docPar, out ksDocumentParam docPar1, out ksRectangleParam par1,
+                out ksRectangleParam model1, out ksRectangleParam model2, out ksRectangleParam model3,
+                out ksRectangleParam model4, out ksRectangleParam model5, out ksRectangleParam model6,
+                out ksRectangleParam model7, out ksRectangleParam model8, out ksRectangleParam model9,
+                out ksRectangleParam model10, out ksRectangleParam model11, out ksRectangleParam model12,
+                out ksRectangleParam model13, out ksRectangleParam model14, out ksRectangleParam model15,
+                out ksRectangleParam model16, out ksRectangleParam model17, out ksRectangleParam model18,
+                out ksRectangleParam model19, out ksRectangleParam model20, out ksRectangleParam model21,
+                out ksMathPointParam Point1, out ksMathPointParam Point2);
+            if ((docPar != null) & (docPar1 != null))
+            {
+                docPar.regime = 0;
+                docPar.type = (short)DocType.lt_DocFragment;
+                doc.ksCreateDocument(docPar);
+                {
+                    Zagotovka(par1);
+                    model1.x = 265; 
+                    model1.y = 140;
+                    model1.height = (par1.height / 2 - 180);
+                    model1.width = (par1.width - 265 - 140 - 80 * 3) / 4;
+                    model1.style = 1;
+                    doc.ksRectangle(model1);
+                    model2.x = (model1.width + 80 + 265);
+                    model2.y = 140;
+                    model2.height = (par1.height / 2 - 180);
+                    model2.width = (par1.width - 265 - 140 - 80 * 3) / 4;
+                    model2.style = 1;
+                    doc.ksRectangle(model2);
+                    model3.x = (model1.width*2 + 80*2 + 265);
+                    model3.y = 140;
+                    model3.height = (par1.height / 2 -180);
+                    model3.width = (par1.width - 265 - 140 - 80 * 3) / 4;
+                    model3.style = 1;
+                    doc.ksRectangle(model3);
+                    model4.x = 265;
+                    model4.y = par1.height/2+40;
+                    model4.height = (par1.height/2 - 180);
+                    model4.width = (par1.width - 265 - 140 - 80 * 3) / 4;
+                    model4.style = 1;
+                    doc.ksRectangle(model4);
+                    model5.x = (model1.width + 80 + 265);
+                    model5.y = par1.height / 2 + 40;
+                    model5.height = (par1.height/2 - 180);
+                    model5.width = (par1.width - 265 - 140 - 80 * 3) / 4;
+                    model5.style = 1;
+                    doc.ksRectangle(model5);
+                    model6.x = (model1.width * 2 + 80 * 2 + 265);
+                    model6.y = par1.height / 2 + 40;
+                    model6.height = (par1.height/2 - 180);
+                    model6.width = (par1.width - 265 - 140 - 80 * 3) / 4;
+                    model6.style = 1;
+                    doc.ksRectangle(model6);
+                    doc.ksLineSeg(265 + model1.width * 3 + 80 * 3, 140,265 + model1.width * 3 + 80 * 3+model1.width-80,140,1);
+                    doc.ksLineSeg(265 + model1.width * 3 + 80 * 3, 140, 265 + model1.width * 3 + 80 * 3, par1.height / 2 - 40,1);
+                    doc.ksLineSeg(265 + model1.width * 3 + 80 * 3, par1.height / 2 - 40,par1.width-140, par1.height / 2 - 40,1);
+                    doc.ksLineSeg(265 + model1.width * 3 + 80 * 3, par1.height/2+40, par1.width - 140, par1.height / 2 + 40, 1);
+                    doc.ksLineSeg(265 + model1.width * 3 + 80 * 3, par1.height / 2 + 40, 265 + model1.width * 3 + 80 * 3,par1.height-140,1);
+                    doc.ksLineSeg(265 + model1.width * 3 + 80 * 3, par1.height - 140, 265 + model1.width * 3 + 80 * 3 + model1.width - 80, par1.height - 140, 1);
+                    doc.ksArcBy3Points(265 + model1.width * 3 + 80 * 3 + model1.width - 80, 140, 265 + model1.width * 3 + 80 * 3 + model1.width-13, par1.height/3, par1.width - 140, par1.height / 2 - 40, 1);
+                    doc.ksArcBy3Points(par1.width - 140, par1.height / 2 + 40, 265 + model1.width * 3 + 80 * 3 + model1.width-13, (par1.height*2) / 3, 265 + model1.width * 3 + 80 * 3 + model1.width - 80, par1.height - 140, 1);
+                }
+            }
         }
 
         public static void Econom80()
@@ -2791,25 +2854,25 @@ namespace Steps.NET
                     model2.x = (model1.width + 80 + 265);
                     model2.y = 140;
                     model2.height = (par1.height - 280);
-                    model2.width = (par1.width - 265 * 2 - 80 * 4) / 5; ;
+                    model2.width = (par1.width - 265 * 2 - 80 * 4) / 5;
                     model2.style = 1;
                     doc.ksRectangle(model2);
                     model3.x = (model1.width*2 + 80*2 + 265);
                     model3.y = 140;
                     model3.height = (par1.height - 280);
-                    model3.width = (par1.width - 265 * 2 - 80 * 4) / 5; ;
+                    model3.width = (par1.width - 265 * 2 - 80 * 4) / 5;
                     model3.style = 1;
                     doc.ksRectangle(model3);
                     model4.x = (model1.width*3 + 80*3 + 265);
                     model4.y = 140;
                     model4.height = (par1.height - 280);
-                    model4.width = (par1.width - 265 * 2 - 80 * 4) / 5; ;
+                    model4.width = (par1.width - 265 * 2 - 80 * 4) / 5;
                     model4.style = 1;
                     doc.ksRectangle(model4);
                     model5.x = (model1.width*4 + 80*4 + 265);
                     model5.y = 140;
                     model5.height = (par1.height - 280);
-                    model5.width = (par1.width - 265 * 2 - 80 * 4) / 5; ;
+                    model5.width = (par1.width - 265 * 2 - 80 * 4) / 5;
                     model5.style = 1;
                     doc.ksRectangle(model5);
                 }
